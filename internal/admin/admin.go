eae957c7d4bf039c1dbf738e4ec4a58a02477f41
SteveYi <jackcooku@steveyi.net>
2022-02-10 23:13:57 +0800

    Add Json Type

--- internal/admin/admin.go
+++ internal/admin/admin.go
@@ -1,10 +1,14 @@
 package admin
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/tidwall/sjson"
 )
@@ -12,33 +16,69 @@ import (
 const Init_Json = `{"POP_List":[],"POP_Info":[]}`
 
 func AddPoP(c *gin.Context) {
-	// Check Json File Available
-	sjson.Set(Init_Json, "POP_List.POP_Name", "POP1")
-	sjson.Set(Init_Json, "POP_List.uuid", "b8ddc37a-7fd9-4236-a54c-181cf3a1549c")
-
 	// TODO: Add PoP
 	Authodized_Key := c.PostForm("Authorized_Key")
 	PoP := c.PostForm("PoP")
 	Router_IP := c.PostForm("MGMT_IP")
 
-	// TODO: Define PoP
-	type PoP_Info struct {
-		PoP string `json:"pop"`
-		IP  string `json:"ip"`
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	// Defind Json Type
+	type List struct {
+		POP_Name string
+		uuid     string
+	}
+	type Info struct {
+		uuid     string
+		mgmt_IP  string
+		Add_Time int
+		status   bool
+	}
+
+	type PoP_Json struct {
+		POP_List []List
+		POP_Info []Info
 	}
 
 	// TODO: Save PoP Information
 	if Authentication(Authodized_Key) {
-		var PoP_Info_List []PoP_Info
-		PoP_Info_List = append(PoP_Info_List, PoP_Info{PoP, Router_IP})
+
+		var j PoP_Json
+
+		// Add PoP List
+		j.POP_List = append(j.POP_List, List{PoP, uuid.String()})
+		sjson.Set(Init_Json, "POP_List.POP_Name", PoP)
+		sjson.Set(Init_Json, "POP_List.uuid", uuid)
+
+		// Add PoP Infomotion
+		j.POP_Info = append(j.POP_Info, Info{uuid: uuid.String(), mgmt_IP: Router_IP, Add_Time: int(time.Now().Unix()), status: true})
+
+		// Save to Json File
+		b, err := json.Marshal(j)
+
+		if err != nil {
+			fmt.Println("json err:", err)
+		}
+		fmt.Println(string(b))
+
 		c.String(200, "Success")
 	} else {
 		c.String(401, "Unauthorized")
 	}
 }
 
+func SaveJsonFile(content string) bool {
+	f, _ := os.Create("pop_info.json")
+	f.WriteString(content)
+	return true
+}
+
 func CheckPoPAvailability(PoP string) bool {
 	// Search Json
+	// Idea: Create a loop to find the PoP?
 	return false
 }
 
